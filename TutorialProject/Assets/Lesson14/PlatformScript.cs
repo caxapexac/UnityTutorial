b42c3a8bf1722f18c95c768bc5e6b2b47e1435d0
caxapexac <caxapexac@gmail.com>
2019-11-15 12:04:49 +0300

    Added lesson 14

---
+++ TutorialProject/Assets/Lesson14/PlatformScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformScript : MonoBehaviour
+{
+    // Скорость вращения
+    public float Speed;
+    
+    // Направление вращения
+    public Vector3 Direction;
+
+    void Update()
+    {
+        // Если кнопка Q не нажата (опциональная возможность остановить вращение)
+        if (Input.GetKeyDown(KeyCode.Q) == false)
+        {
+            // Вращаем объект
+            transform.Rotate(Direction * Speed * Time.deltaTime);
+        }
+    }
+}
