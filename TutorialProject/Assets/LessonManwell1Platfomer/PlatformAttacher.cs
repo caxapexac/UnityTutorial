20a3fc3a7949d82a5ad1fc4fadf8b458e1c9f842
caxapexac <caxapexac@gmail.com>
2019-11-29 23:38:37 +0300

    LessonManwell1Added

---
+++ TutorialProject/Assets/LessonManwell1Platfomer/PlatformAttacher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Скрипт для прилипания игрока к платформе
+public class PlatformAttacher : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // Чтобы к платформе прилипал только игрок
+        {
+            other.transform.parent = transform; // transform - позиция платформы
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player")) // Чтобы к платформе прилипал только игрок
+        {
+            other.transform.parent = null; //См. иерархию, чтобы понять что происходит
+        }
+    }
+}
---
+++ TutorialProject/Assets/LessonManwell1Platfomer/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector3 StartPoint;
+
+    private void Start()
+    {
+        StartPoint = transform.position;
+    }
+
+    public void Restart()
+    {
+        transform.position = StartPoint;
+    }
+}
+
